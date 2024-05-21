ed9f8e95e1d0dcc2803af7e4d908bc1500844ab6
Vitor Cavalcante <101594191+vitorjpc10@users.noreply.github.com>
2024-05-21 18:53:50 -0400

    Added Airflow dag

---
+++ dbt_dag/dags/data_pipeline_snowflake/models/marts/int_order_items_summary.sql
@@ -0,0 +1,9 @@
+select
+    order_key,
+    sum(extended_price) as gross_item_sales_amount,
+    avg(discount_percentage) as avg_item_discount_percentage_amount,
+    sum(item_discount_amount) as item_discount_amount
+from
+    {{ ref('int_order_items') }}
+group by
+    order_key
